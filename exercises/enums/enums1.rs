d8e7ff6cb8c6e85461eef16156a563ebcaf4057a
nephrencake <834957063@qq.com>
2023-10-09 21:54:10 +0800

    day1

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,11 +2,13 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit = 10,
+    Echo = 11,
+    Move = 12,
+    ChangeColor = 13,
 }
 
 fn main() {
